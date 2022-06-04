ee4e8f03a531af52c79a8ca42794d66a0fd11624
Kaique Sacchi <sacchikaique@gmail.com>
2022-06-04 16:14:53 -0300

    api: added end-point to retrieve shortened link

---
+++ src/pages/api/getLink/[slug].ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@lib/prisma";
+
+type Data = {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  slug: string;
+  fullLink: string;
+};
+
+type Error = {
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | Error>
+) {
+  const slug = req.query.slug;
+
+  if (typeof slug !== "string") {
+    res.status(400).json({ message: "only one slug allowed" });
+    return;
+  }
+
+  const data = await prisma.link.findUnique({ where: { slug } });
+
+  if (!data) {
+    res.status(404).json({ message: "not found" });
+    return;
+  }
+
+  res.status(200).json(data);
+}
