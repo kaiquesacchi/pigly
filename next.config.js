447abdc229cf32c857231b75243f3f4c4edc7bab
Kaique Sacchi <sacchikaique@gmail.com>
2022-06-04 22:13:39 -0300

    yarn: added sentry; sentry's initial setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+// References:
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+const { withSentryConfig } = require('@sentry/nextjs');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const sentryWebpackPluginOptions = {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+  silent: true, // Suppresses all logs
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
