337d5a3cf3f4cc2e8c7d63e719f0aae6a33c4d7e
Kaique Sacchi <sacchikaique@gmail.com>
2022-06-19 20:32:21 -0300

    yarn: added react-toastify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import type { AppProps } from "next/app";
 import "@/styles/global.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <ToastContainer />
+    </>
+  );
 }
 
 export default MyApp;
